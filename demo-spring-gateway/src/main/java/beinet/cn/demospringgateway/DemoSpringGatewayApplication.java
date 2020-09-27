05fd77e1b1c6331daf4aa83e266f2ffb52cb7810
youbl <youbl@126.com>
2020-09-27 16:39:54 +0800

    SpringCloudGateway网关新建，通过配置文件进行转发

---
+++ demo-spring-gateway/src/main/java/beinet/cn/demospringgateway/DemoSpringGatewayApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.demospringgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringGatewayApplication.class, args);
+    }
+
+}
