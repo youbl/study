faf7bb8556119d6784365390d167da2ddd87b046
youbl <youbl@126.com>
2020-09-27 18:22:42 +0800

    SpringCloudGateway网关：转发请求前 读取请求的Header和请求url、参数，以便做一些特殊处理。

--- demo-spring-gateway/src/main/java/beinet/cn/demospringgateway/BeinetFilter.java
+++ demo-spring-gateway/src/main/java/beinet/cn/demospringgateway/BeinetFilter.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -18,15 +19,20 @@ public class BeinetFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return chain.filter(exchange.mutate().request(builder -> {
             //ServerHttpRequest.Builder builder = exchange.getRequest().mutate();
-            addTimeToRequestHeader(builder);
+            addTimeToRequestHeader(exchange, builder);
             builder.build();
         }).build())
-                .then(Mono.fromRunnable(() -> {
-                    addTimeToResponseHeader(exchange);
-                }));
+                .then(Mono.fromRunnable(() -> addTimeToResponseHeader(exchange)));
     }
 
-    private void addTimeToRequestHeader(ServerHttpRequest.Builder builder) {
+    private void addTimeToRequestHeader(ServerWebExchange exchange, ServerHttpRequest.Builder builder) {
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders(); // 读取Header，不需要转发，网关会自动转发
+
+        String url = String.valueOf(request.getURI()); // 带有完整查询串
+        // MultiValueMap<String, String> query = request.getQueryParams();
+        builder.header("beinet.url", url);
+
         builder.header("beinet.request.now", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 
