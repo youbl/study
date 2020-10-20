e76a2b2920af6d4b00a3b4c24074c5be9595b25e
youbl <youbl@126.com>
2020-10-20 16:22:54 +0800

    改用内存Redis，以便支持单元测试

--- demo-log-redis/src/test/java/beinet/cn/logdemoredis/LogdemoRedisApplicationTests.java
+++ demo-log-redis/src/test/java/beinet/cn/logdemoredis/LogdemoRedisApplicationTests.java
@@ -2,10 +2,18 @@ package beinet.cn.logdemoredis;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
 
 @SpringBootTest
 class LogdemoRedisApplicationTests {
+    @Qualifier("redisTemplate")
+    @Autowired
+    RedisTemplate redis;
 
     @Before
     void doBefore() {
@@ -14,6 +22,13 @@ class LogdemoRedisApplicationTests {
 
     @Test
     void contextLoads() {
+        redis.opsForValue().setIfAbsent("bbb", "ddd");
+        redis.opsForValue().set("aaa", "bbb", Duration.ofMinutes(1));
+        Object ret = redis.opsForValue().get("aaa");
+
+        redis.delete("bbb");
+
+        redis.opsForHash().get("abc", "def");
     }
 
 }
