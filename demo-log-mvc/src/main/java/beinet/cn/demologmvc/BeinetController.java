8a7ebbffed1f886f79485633e034684df57515c0
youbl <youbl@126.com>
2020-10-21 10:26:54 +0800

    添加post日志

--- demo-log-mvc/src/main/java/beinet/cn/demologmvc/BeinetController.java
+++ demo-log-mvc/src/main/java/beinet/cn/demologmvc/BeinetController.java
@@ -1,7 +1,10 @@
 package beinet.cn.demologmvc;
 
+import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.zalando.logbook.DefaultHttpLogFormatter;
 import org.zalando.logbook.HttpLogFormatter;
@@ -13,12 +16,25 @@ import java.time.format.DateTimeFormatter;
 public class BeinetController {
     @GetMapping("time")
     public String time() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+        return '"' + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")) + '"';
     }
 
+    @PostMapping("add")
+    public Dto add(@RequestBody Dto dto) {
+        dto.setName("Hello, " + dto.getName());
+        dto.setTime(LocalDateTime.now());
+        return dto;
+    }
 
     @Bean
     public HttpLogFormatter httpLogFormatter() {
         return new DefaultHttpLogFormatter();
+    }
+
+    @Data
+    static class Dto {
+        private int id;
+        private String name;
+        private LocalDateTime time;
     }
 }
