16579467feb87ecdcbfea214daaf4faaedf41560
youbl <youbl@126.com>
2020-11-17 17:45:43 +0800

    移除旧的单元测试demo

--- demo-jpa-unittest/src/main/java/beinet/cn/demojpaunittest/repository/AaaRepository.java
+++
@@ -1,7 +0,0 @@
-package beinet.cn.demojpaunittest.repository;
-
-import beinet.cn.demojpaunittest.entity.Aaa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AaaRepository extends JpaRepository<Aaa, Long> {
-}
