7bbfbc32462dcb7727a54b0b7c772a5b29a304dc
youbl <youbl@126.com>
2021-03-23 16:13:03 +0800

    SpringCloudGateway学习项目：feign去掉url配置，改用eureka注册中心的应用名进行访问

--- demo-spring-cloud/demo-mall/src/main/java/beinet/cn/demomall/FeignProduct.java
+++ demo-spring-cloud/demo-mall/src/main/java/beinet/cn/demomall/FeignProduct.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "feignProduct", url = "http://localhost:9004/product")
+@FeignClient(name = "demo-product")  // 这个demo-product 必须是产品项目注册到Eureka里的应用名
 public interface FeignProduct {
-    @GetMapping("{id}")
+    @GetMapping("product/{id}")
     ProductDto get(@PathVariable int id);
 }
