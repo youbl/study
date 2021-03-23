060093c49f9a89dae1cbb69fb105d61f9b87db44
youbl <youbl@126.com>
2021-03-23 16:05:38 +0800

    SpringCloudGateway学习项目：添加网关项目，并配置路由到商城项目和产品项目

---
+++ demo-spring-cloud/demo-gateway-server/src/main/java/beinet/cn/demogatewayserver/DemoGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.demogatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGatewayServerApplication.class, args);
+    }
+
+}
---
+++ demo-spring-cloud/demo-gateway-server/src/test/java/beinet/cn/demogatewayserver/DemoGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package beinet.cn.demogatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
