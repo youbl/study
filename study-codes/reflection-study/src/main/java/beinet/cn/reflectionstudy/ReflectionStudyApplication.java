1b66077a75db7a2f87aad1488196adfe09734b37
youbl <youbl@126.com>
2021-04-09 11:34:56 +0800

    Reflection学习项目：获取不同方法的返回值类型，并尝试进行反序列化，得到正确结果

--- study-codes/reflection-study/src/main/java/beinet/cn/reflectionstudy/ReflectionStudyApplication.java
+++ study-codes/reflection-study/src/main/java/beinet/cn/reflectionstudy/ReflectionStudyApplication.java
@@ -1,5 +1,7 @@
 package beinet.cn.reflectionstudy;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -7,9 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -25,6 +27,14 @@ public class ReflectionStudyApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         showAllMethodInfo();
+        System.out.println();
+        testConvert("DtoMethodArray");
+        testConvert("DtoMethodList");
+        testConvert("LongMethodArray1");
+        testConvert("LongMethodArray2");
+        testConvert("LongMethodList");
+        testConvert("LongMethodMap1");
+        testConvert("LongMethodMap2");
     }
 
     void showAllMethodInfo() {
@@ -43,14 +53,121 @@ public class ReflectionStudyApplication implements CommandLineRunner {
                 method.getName().equals("equals")) {
             return;
         }
+
         Class<?> clzz = method.getReturnType();
-        System.out.println(method + "\n  返回类型: " + clzz + " 数组:" + clzz.isArray()
-                + "\n  组件类型: " + clzz.getComponentType()
-                + "\n  泛型类型: " + method.getGenericReturnType());
+        System.out.println(method + "\n  返回类型: " + getTypeStr(clzz)
+                + "\n  isArray:" + clzz.isArray()
+                + "\n  isCollection:" + Collection.class.isAssignableFrom(clzz)
+                + "\n  isMap:" + Map.class.isAssignableFrom(clzz)
+                + "\n  ComponentType: " + getTypeStr(clzz.getComponentType())
+                + "\n  method.GenericType: " + getTypeStr(method.getGenericReturnType()));
     }
 
     List<Method> getMethods() {
         return Arrays.stream(SomeMethod.class.getMethods()).sorted(Comparator.comparing(Method::getName)).collect(Collectors.toList());
     }
 
+    String getTypeStr(Type type) {
+        if (type == null)
+            return "--null--";
+        return type + " \t\t【" + type.getClass() + "】";
+    }
+
+    void testConvert(String methodName) throws Exception {
+        SomeMethod someMethod = new SomeMethod();
+        Method method = someMethod.getClass().getMethod(methodName, null);
+        Class returnType = method.getReturnType();
+        Object obj1 = method.invoke(someMethod);
+        Object obj2 = convert(obj1, getReturnType(method));
+        System.out.println(returnType + " : " + obj1.getClass() + " : " + obj2.getClass());
+    }
+
+    /**
+     * 返回方法的返回值类型，用于反序列化
+     *
+     * @param method 方法
+     * @return 类型
+     */
+    private Type getReturnType(Method method) {
+        Class methodReturnType = method.getReturnType();
+        if (methodReturnType.isArray()) {
+            // 数组，类似 long[] Long[] 直接返回，不影响反序列化
+            return methodReturnType;
+        }
+
+        Type type = getCollectionType(method);
+        if (type != null) {
+            // List HashSet等类型
+            return type;
+        }
+        type = getMapType(method);
+        if (type != null) {
+            return type;
+        }
+        return methodReturnType;
+    }
+
+    /**
+     * 如果是Collection类型，则返回带泛型的类型
+     *
+     * @param method 方法
+     * @return 类型
+     */
+    private JavaType getCollectionType(Method method) {
+        Class methodReturnType = method.getReturnType();
+        if (!Collection.class.isAssignableFrom(methodReturnType)) {
+            return null;
+        }
+        Type type = method.getGenericReturnType();
+        if (type != null && type instanceof ParameterizedType) {
+            Type[] arrType = ((ParameterizedType) type).getActualTypeArguments();
+            if (arrType == null || arrType.length < 1) {
+                return null;
+            }
+            return mapper.getTypeFactory().constructCollectionType(methodReturnType, (Class) arrType[0]);
+        }
+        return null;
+    }
+
+    /**
+     * 如果是Map类型，则返回带泛型的类型
+     *
+     * @param method 方法
+     * @return 类型
+     */
+    private JavaType getMapType(Method method) {
+        Class methodReturnType = method.getReturnType();
+        if (!Map.class.isAssignableFrom(methodReturnType)) {
+            return null;
+        }
+        Type type = method.getGenericReturnType();
+        if (type != null && type instanceof ParameterizedType) {
+            Type[] arrType = ((ParameterizedType) type).getActualTypeArguments();
+            if (arrType == null || arrType.length < 2) {
+                return null;
+            }
+            return mapper.getTypeFactory().constructMapType(methodReturnType, (Class) arrType[0], (Class) arrType[1]);
+        }
+        return null;
+    }
+
+    /**
+     * 把对象转换为指定的类型并返回
+     *
+     * @param obj        对象
+     * @param returnType 类型
+     * @return 转换结果
+     * @throws JsonProcessingException 异常
+     */
+    private Object convert(Object obj, Type returnType) throws JsonProcessingException {
+        if (obj == null) {
+            return null;
+        }
+        if (returnType instanceof Class) {
+            return mapper.readValue(mapper.writeValueAsString(obj), (Class) returnType);
+        } else if (returnType instanceof JavaType) {
+            return mapper.readValue(mapper.writeValueAsString(obj), (JavaType) returnType);
+        }
+        return obj;
+    }
 }
--- study-codes/reflection-study/src/main/java/beinet/cn/reflectionstudy/SomeMethod.java
+++ study-codes/reflection-study/src/main/java/beinet/cn/reflectionstudy/SomeMethod.java
@@ -1,10 +1,7 @@
 package beinet.cn.reflectionstudy;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SomeMethod {
 //    public long LongMethod1() {
@@ -15,28 +12,46 @@ public class SomeMethod {
 //        return 1L;
 //    }
 
-    public List<Long> LongMethodArr1() {
+    public Map<Long, Dto> LongMethodMap1() {
+        return LongMethodMap2();
+    }
+
+    public HashMap<Long, Dto> LongMethodMap2() {
+        HashMap<Long, Dto> ret = new HashMap<>();
+        Dto dto = new Dto();
+        dto.setId(123);
+        dto.setName("aaa");
+        ret.put(123L, dto);
+
+        dto = new Dto();
+        dto.setId(456);
+        dto.setName("bbb");
+        ret.put(1234L, dto);
+        return ret;
+    }
+
+    public List<Long> LongMethodList() {
         List<Long> ret = new ArrayList<>();
         ret.add(123L);
         ret.add(1234L);
         return ret;
     }
 
-    public Long[] LongMethodArr2() {
+    public Long[] LongMethodArray1() {
         Long[] ret = new Long[2];
         ret[0] = 123L;
         ret[1] = 456L;
         return ret;
     }
 
-    public long[] LongMethodArr3() {
+    public long[] LongMethodArray2() {
         long[] ret = new long[2];
         ret[0] = 123L;
         ret[1] = 456L;
         return ret;
     }
 
-    public List<Dto> DtoMethodArr1() {
+    public List<Dto> DtoMethodList() {
         Dto[] ret = new Dto[2];
         ret[0] = new Dto();
         ret[0].setId(123);
@@ -50,7 +65,7 @@ public class SomeMethod {
         return arr;// Arrays.asList(ret);
     }
 
-    public Dto[] DtoMethodArr2() {
+    public Dto[] DtoMethodArray() {
         Dto[] ret = new Dto[2];
         ret[0] = new Dto();
         ret[0].setId(123);
