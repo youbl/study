14dfddb6ea39bb39a852e1c6e50d126e1d72bcba
youbl <youbl@126.com>
2021-07-01 23:56:21 +0800

    代码学习与试验项目：打包有问题，添加plugin定义

--- study-codes/canal-demo/src/main/java/beinet/cn/canalDemo/eventHandler/MySQLHandler.java
+++ study-codes/canal-demo/src/main/java/beinet/cn/canalDemo/eventHandler/MySQLHandler.java
@@ -3,22 +3,24 @@ package beinet.cn.canalDemo.eventHandler;
 import com.alibaba.otter.canal.protocol.CanalEntry;
 import com.xpand.starter.canal.annotation.*;
 
+import java.time.LocalDateTime;
+
 @CanalEventListener
 public class MySQLHandler {
-    @InsertListenPoint
-    public void onInsertEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
-        outputMsg(eventType, rowData, "onInsertEvent");
-    }
-
-    @UpdateListenPoint
-    public void onUpdateEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
-        outputMsg(eventType, rowData, "onUpdateEvent");
-    }
-
-    @DeleteListenPoint
-    public void onDeleteEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
-        outputMsg(eventType, rowData, "onDeleteEvent");
-    }
+//    @InsertListenPoint
+//    public void onInsertEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+//        outputMsg(eventType, rowData, "onInsertEvent");
+//    }
+//
+//    @UpdateListenPoint
+//    public void onUpdateEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+//        outputMsg(eventType, rowData, "onUpdateEvent");
+//    }
+//
+//    @DeleteListenPoint
+//    public void onDeleteEvent(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+//        outputMsg(eventType, rowData, "onDeleteEvent");
+//    }
 
     @ListenPoint
 //(destination = "example", schema = "canal-test", table = {"t_user", "test_table"}, eventType = CanalEntry.EventType.UPDATE)
@@ -27,7 +29,7 @@ public class MySQLHandler {
     }
 
     private void outputMsg(CanalEntry.EventType eventType, CanalEntry.RowData rowData, String title) {
-        System.out.println(title + " 收到事件:" + eventType);
+        System.out.println(LocalDateTime.now() + " " + title + " 收到事件:" + eventType);
         for (CanalEntry.Column column : rowData.getBeforeColumnsList()) {
             System.out.println(title + ":BEFORE  " + column.getName() + ":" + column.getValue());
         }
