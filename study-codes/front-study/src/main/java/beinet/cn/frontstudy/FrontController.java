f5ce9ba96a72e52b488d4518daeaea1f185a8265
youbl <youbl@126.com>
2021-07-05 12:50:41 +0800

    代码学习与试验项目：模板引擎thymeleaf的循环展示数据

--- study-codes/front-study/src/main/java/beinet/cn/frontstudy/FrontController.java
+++ study-codes/front-study/src/main/java/beinet/cn/frontstudy/FrontController.java
@@ -1,8 +1,14 @@
 package beinet.cn.frontstudy;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class FrontController {
@@ -10,6 +16,19 @@ public class FrontController {
     public String firstDemo(Model model) {
         model.addAttribute("title", "Thymeleaf页面演示");
         model.addAttribute("body", "<b>Hello thymeleaf.</b>");
+
+        List<User> datas = new ArrayList<>();
+        datas.add(new User(11, "张三"));
+        datas.add(new User(121, "李四"));
+        model.addAttribute("dataList", datas);
         return "firstTh";
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class User {
+        private int id;
+        private String name;
     }
 }
