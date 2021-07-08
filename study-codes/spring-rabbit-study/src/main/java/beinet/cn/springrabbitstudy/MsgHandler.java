8001158a7c95b1b6fd64d5788005b4ea130e9b69
youbl <youbl@126.com>
2021-07-08 14:47:15 +0800

    代码学习与试验项目：Spring RabbitMQ使用注解来创建Exchange、Queue和Binding

--- study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/MsgHandler.java
+++ study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/MsgHandler.java
@@ -1,6 +1,9 @@
 package beinet.cn.springrabbitstudy;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +15,12 @@ public class MsgHandler {
     第1次访问：http://localhost:8801/publish?msg=abc 输出 46收到消息aaa:(Body:'PUBLISH:abc'
     第2次访问：http://localhost:8801/publish?msg=abc 输出 46收到消息xxx:(Body:'PUBLISH:abc'
      */
-    @RabbitListener(queues = RabbitOperator.QUEUE)
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue(value = RabbitOperator.QUEUE, durable = "true"),
+                    exchange = @Exchange(value = RabbitOperator.EXCHANGE, type = "direct", durable = "true", autoDelete = "false"),
+                    key = RabbitOperator.ROUTEKEY,
+                    declare = "true")
+    })
     void xx1(Message message) {
         System.out.println(Thread.currentThread().getId() + "收到消息aaa:" + message);
     }
--- study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/RabbitOperator.java
+++ study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/RabbitOperator.java
@@ -13,9 +13,9 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RabbitOperator {
-    private static final String EXCHANGE = "test.beinet.exg";
+    static final String EXCHANGE = "test.beinet.exg";
     static final String QUEUE = "test.beinet.queue";
-    private static final String ROUTEKEY = "test.beinet.route";
+    static final String ROUTEKEY = "test.beinet.route";
 
     private final RabbitTemplate template;
 
@@ -23,39 +23,40 @@ public class RabbitOperator {
         this.template = template;
     }
 
-    /**
-     * 不加Bean注解，将不会自动创建Exchange
-     *
-     * @return
-     */
-    @Bean("beinet.exg")
-    public DirectExchange createExchange() {
-        // 创建 持久化的，不自动删除的Exchange
-        return new DirectExchange(EXCHANGE, true, false);
-    }
+    // 通过 MsgHandler.java 里的注解，来创建Exchange、Queue、Binding
+//    /**
+//     * 不加Bean注解，将不会自动创建Exchange
+//     *
+//     * @return
+//     */
+//    @Bean("beinet.exg")
+//    public DirectExchange createExchange() {
+//        // 创建 持久化的，不自动删除的Exchange
+//        return new DirectExchange(EXCHANGE, true, false);
+//    }
 
-    /**
-     * 不加Bean注解，将不会自动创建Queue
-     *
-     * @return
-     */
-    @Bean("beinet.queue")
-    public Queue createQueue() {
-        // 创建持久化的Queue
-        return new Queue(QUEUE, true);
-    }
+//    /**
+//     * 不加Bean注解，将不会自动创建Queue
+//     *
+//     * @return
+//     */
+//    @Bean("beinet.queue")
+//    public Queue createQueue() {
+//        // 创建持久化的Queue
+//        return new Queue(QUEUE, true);
+//    }
 
 
-    /**
-     * 不加Bean注解，将不会自动创建Binding.
-     * 注：虽然这里会调用 createQueue 和 createExchange方法，但是并不会触发创建
-     *
-     * @return
-     */
-    @Bean("beinet.bind")
-    public Binding doBind() {
-        return BindingBuilder.bind(createQueue()).to(createExchange()).with(ROUTEKEY);
-    }
+//    /**
+//     * 不加Bean注解，将不会自动创建Binding.
+//     * 注：虽然这里会调用 createQueue 和 createExchange方法，但是并不会触发创建
+//     *
+//     * @return
+//     */
+//    @Bean("beinet.bind")
+//    public Binding doBind() {
+//        return BindingBuilder.bind(createQueue()).to(createExchange()).with(ROUTEKEY);
+//    }
 
     /**
      * 发布消息
