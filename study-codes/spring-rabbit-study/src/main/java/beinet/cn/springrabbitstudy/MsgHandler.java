c2d87247d203f05a3971870c3015476c6a92a63e
youbl <youbl@126.com>
2021-07-08 14:16:06 +0800

    代码学习与试验项目：Spring RabbitMQ的一个项目多次绑定一个Queue时，是轮流消费的

--- study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/MsgHandler.java
+++ study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/MsgHandler.java
@@ -6,11 +6,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MsgHandler {
+    /*
+    下面2个方法，监听同一个队列，会轮流消费，而不是1个消息消费2次。
+    比如：
+    第1次访问：http://localhost:8801/publish?msg=abc 输出 46收到消息aaa:(Body:'PUBLISH:abc'
+    第2次访问：http://localhost:8801/publish?msg=abc 输出 46收到消息xxx:(Body:'PUBLISH:abc'
+     */
     @RabbitListener(queues = RabbitOperator.QUEUE)
     void xx1(Message message) {
         System.out.println(Thread.currentThread().getId() + "收到消息aaa:" + message);
     }
-
 
     @RabbitListener(queues = RabbitOperator.QUEUE)
     void xx2(Message message) {
