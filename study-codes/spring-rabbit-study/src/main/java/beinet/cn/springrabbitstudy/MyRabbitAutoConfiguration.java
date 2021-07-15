00c274dad201b950a723be3c0840261b4e883656
youbl <youbl@126.com>
2021-07-15 15:04:13 +0800

    代码学习与试验项目：Spring RabbitMQ运行时拦截Queue的消费方法，前面的processAmqpListener方案有问题，不能传递旧的Method

--- study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/MyRabbitAutoConfiguration.java
+++ study-codes/spring-rabbit-study/src/main/java/beinet/cn/springrabbitstudy/MyRabbitAutoConfiguration.java
@@ -1,9 +1,16 @@
 package beinet.cn.springrabbitstudy;
 
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerAnnotationBeanPostProcessor;
 import org.springframework.amqp.rabbit.config.RabbitListenerConfigUtils;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,5 +35,28 @@ public class MyRabbitAutoConfiguration {
     @Bean(name = RabbitListenerConfigUtils.RABBIT_LISTENER_ENDPOINT_REGISTRY_BEAN_NAME)
     public RabbitListenerEndpointRegistry rabbitListenerEndpointRegistry() {
         return new RabbitListenerEndpointRegistry();
+    }
+
+    // 复制 org.springframework.boot.autoconfigure.amqp.RabbitAnnotationDrivenConfiguration里，
+    // 初始化SimpleRabbitListenerContainerFactory的代码
+    @Bean(name = "rabbitListenerContainerFactory")
+    @ConditionalOnProperty(prefix = "spring.rabbitmq.listener", name = "type", havingValue = "simple",
+            matchIfMissing = true)
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            SimpleRabbitListenerContainerFactoryConfigurer configurer, ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setConnectionFactory(connectionFactory);
+//        factory.setConcurrentConsumers(3);
+//        factory.setMaxConcurrentConsumers(10);
+//        factory.setContainerCustomizer(container -> /* customize the container */);
+        factory.setAfterReceivePostProcessors(new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                message.getMessageProperties().getHeaders().put("newHeader1", "newValue");
+                return message;
+            }
+        });
+        return factory;
     }
 }
