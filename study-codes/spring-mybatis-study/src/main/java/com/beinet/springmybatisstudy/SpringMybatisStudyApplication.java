cec2fbbe055115428861a5e5ed7012a422a892b6
youbl <youbl@126.com>
2021-08-21 10:48:31 +0800

    Spring-Mybatis学习项目：完成依赖引入，以及第一个Mapper获取列表

--- study-codes/spring-mybatis-study/src/main/java/com/beinet/springmybatisstudy/SpringMybatisStudyApplication.java
+++ study-codes/spring-mybatis-study/src/main/java/com/beinet/springmybatisstudy/SpringMybatisStudyApplication.java
@@ -1,18 +1,37 @@
 package com.beinet.springmybatisstudy;
 
+import com.beinet.springmybatisstudy.entity.User;
+import com.beinet.springmybatisstudy.mapper.UserMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringMybatisStudyApplication implements CommandLineRunner {
+    // 官网 http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/
+    private final UserMapper userMapper;
+
+    public SpringMybatisStudyApplication(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMybatisStudyApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        System.out.printf("我是Mybatis演示");
+    public void run(String... args) {
+        System.out.println("我是Mybatis演示");
+
+        List<User> userList = userMapper.findByAgeGt(20);
+        System.out.println("大于20的人数:" + userList.size());
+        userList.forEach(System.out::println);
+
+        userList = userMapper.findByAgeLet(20);
+        System.out.println("小于等于20的人数:" + userList.size());
+        userList.forEach(System.out::println);
     }
 }
