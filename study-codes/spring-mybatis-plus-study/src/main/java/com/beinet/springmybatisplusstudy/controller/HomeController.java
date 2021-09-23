6be6c5a543d4e6817e7fa19bc710a3f8310483aa
youbl <youbl@126.com>
2021-09-23 18:53:38 +0800

    Spring-Mybatis-Plus学习项目：动态查询效果实现

--- study-codes/spring-mybatis-plus-study/src/main/java/com/beinet/springmybatisplusstudy/controller/HomeController.java
+++ study-codes/spring-mybatis-plus-study/src/main/java/com/beinet/springmybatisplusstudy/controller/HomeController.java
@@ -1,9 +1,16 @@
 package com.beinet.springmybatisplusstudy.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.beinet.springmybatisplusstudy.dal.entity.User;
 import com.beinet.springmybatisplusstudy.dal.mapper.UserMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * Description:
@@ -20,7 +27,33 @@ public class HomeController {
     }
 
     @GetMapping("")
-    public User index() {
-        return userMapper.selectById(3);
+    public String index() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"))
+                + " " + this.getClass().getName();
     }
+
+    @GetMapping("user/{id}")
+    public User getById(@PathVariable int id) {
+        return userMapper.selectById(id);
+    }
+
+    /**
+     * 返回全部用户
+     *
+     * @return
+     */
+    @GetMapping("user/all")
+    public List<User> getAll() {
+        return userMapper.selectList(null);
+    }
+
+    @GetMapping("user")
+    public List<User> getByCond(@RequestParam int age, @RequestParam String name) {
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.ge("age", age);
+        wrapper.eq("name", name);
+        return userMapper.selectList(wrapper);
+    }
+
+
 }
--- study-codes/spring-mybatis-plus-study/src/main/resources/db/data-h2.sql
+++ study-codes/spring-mybatis-plus-study/src/main/resources/db/data-h2.sql
@@ -3,4 +3,5 @@ VALUES (1, 'Jone', 18, 'test1@baomidou.com'),
        (2, 'Jack', 20, 'test2@baomidou.com'),
        (3, 'Tom', 28, 'test3@baomidou.com'),
        (4, 'Sandy', 21, 'test4@baomidou.com'),
-       (5, 'Billie', 24, 'test5@baomidou.com');
+       (5, 'Billie', 24, 'test5@baomidou.com'),
+       (9, 'ybl', 21, 'abc5@baomidou.com');
