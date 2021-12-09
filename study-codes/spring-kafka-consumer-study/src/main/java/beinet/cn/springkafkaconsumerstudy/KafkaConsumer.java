dfd08a42ad15913ece26334927ba3db16031851b
youbl <youbl@126.com>
2021-12-09 15:15:13 +0800

    Spring-kafka学习项目：消费者项目用正则监听topic

--- study-codes/spring-kafka-consumer-study/src/main/java/beinet/cn/springkafkaconsumerstudy/KafkaConsumer.java
+++ study-codes/spring-kafka-consumer-study/src/main/java/beinet/cn/springkafkaconsumerstudy/KafkaConsumer.java
@@ -18,7 +18,8 @@ public class KafkaConsumer {
 
     // KafkaListener如果设置了属性id，则id将作为groupId使用（会覆盖yml里的group-id），给kafka保存offset；可以设置属性idIsGroup=false
     @KafkaListener(id = "handler1", idIsGroup = false,
-            topics = {"${spring.kafka.template.default-topic}"},
+            //topics = {"${spring.kafka.template.default-topic}"},  // topics写入一个个的topic
+            topicPattern = "task_execute_result.*",                 // topicPattern使用正则匹配topic名称
             containerFactory = "myKafkaFactory")
     // 注意，必须设置factory的AckMode为AckMode.MANUAL_IMMEDIATE 才能会出现这个Acknowledgment参数
     public void msgHandler(List<ConsumerRecord> message, Acknowledgment ack) {
@@ -26,8 +27,8 @@ public class KafkaConsumer {
         try {
             for (ConsumerRecord record : message) {
                 System.out.println(record);
-                if (errIndex > 5)
-                    throw new Exception("aabbcc");
+//                if (errIndex > 5)
+//                    throw new Exception("aabbcc");
                 errIndex++;
             }
             ack.acknowledge(); // 提交偏移量
