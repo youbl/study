a2374e2222fa1260485746f93b3e109628382317
youbl <youbl@126.com>
2021-12-09 10:08:06 +0800

    配置文件内容加密示例：避免在配置文件里暴露明文密码信息

--- study-codes/spring-jpa-study/src/main/java/beinet/cn/springjpastudy/SpringJpaStudyApplication.java
+++ study-codes/spring-jpa-study/src/main/java/beinet/cn/springjpastudy/SpringJpaStudyApplication.java
@@ -1,5 +1,6 @@
 package beinet.cn.springjpastudy;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +10,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SpringJpaStudyApplication {
 
     public static void main(String[] args) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //加密所需的salt
+        textEncryptor.setPassword("123456");
+        //要加密的数据（数据库的用户名或密码）
+        String username = "beinet";
+        String password = "beinet.cnbeinet.cn";
+        System.out.println("username:" + username + " 加密后: " + textEncryptor.encrypt(username));
+        System.out.println("password:" + password + " 加密后: " + textEncryptor.encrypt(password));
+
         SpringApplication.run(SpringJpaStudyApplication.class, args);
     }
 
