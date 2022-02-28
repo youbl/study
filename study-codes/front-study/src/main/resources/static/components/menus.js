49d02adff38f826666a033e297dce7f683ca7bfd
youbl <youbl@126.com>
2022-02-28 17:11:20 +0800

    Vue组件：第二个菜单组件，引用其它的js实现

---
+++ study-codes/front-study/src/main/resources/static/components/menus.js
@@ -0,0 +1,39 @@
+Vue.component('top-menu', {
+    data: function () {
+        return {
+            title: '我是top-menu组件的Title',
+            activeIndex: '1',
+            activeIndex2: '1',
+        }
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    },
+    // 在template的value的前后添加文字，都无法显示？？
+    template: `<div><el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1">{{title}}</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">我的工作台</template>
+    <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-submenu index="2-4">
+      <template slot="title">选项4</template>
+      <el-menu-item index="2-4-1">选项1</el-menu-item>
+      <el-menu-item index="2-4-2">选项2</el-menu-item>
+      <el-menu-item index="2-4-3">选项3</el-menu-item>
+    </el-submenu>
+  </el-submenu>
+  <el-menu-item index="3" disabled>消息中心</el-menu-item>
+  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+</el-menu></div>`,
+});
