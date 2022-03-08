3ef92689ecf2298bddf5eb181ce330f3512812b1
youbl <youbl@126.com>
2022-03-08 20:17:44 +0800

    mapstruct演示项目：新建，无其它依赖

---
+++ study-codes/mapstruct-demo/src/main/java/beinet/cn/mapstructdemo/MapstructDemoApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.mapstructdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapstructDemoApplication.class, args);
+    }
+
+}
