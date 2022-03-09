e2e2a70ff0ee017bfe82a2bc68dcb1933204b106
youbl <youbl@126.com>
2022-03-09 16:54:04 +0800

    spring的邮件demo项目：新建，无引用

---
+++ study-codes/spring-mail-demo/src/main/java/beinet/cn/springmaildemo/SpringMailDemoApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.springmaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMailDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMailDemoApplication.class, args);
+    }
+
+}
---
+++ study-codes/spring-mail-demo/src/test/java/beinet/cn/springmaildemo/SpringMailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package beinet.cn.springmaildemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMailDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
