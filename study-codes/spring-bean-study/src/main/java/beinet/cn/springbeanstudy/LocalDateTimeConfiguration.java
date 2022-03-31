18fe41fa4e1f3f2ceed5da97c63f3c6eef6a7453
youbl <youbl@126.com>
2022-03-31 11:29:09 +0800

    RestController上，接收多种格式的字符串转换为 LocalDateTime

--- study-codes/spring-bean-study/src/main/java/beinet/cn/springbeanstudy/LocalDateTimeConfiguration.java
+++ study-codes/spring-bean-study/src/main/java/beinet/cn/springbeanstudy/LocalDateTimeConfiguration.java
@@ -9,6 +9,10 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_TIME;
 
 /**
  * Description:
@@ -18,13 +22,44 @@ import java.time.format.DateTimeFormatter;
  */
 @Configuration
 public class LocalDateTimeConfiguration {
+    // 支持多种格式反序列化
+    // yyyy-MM-dd HH:mm:ss
+    // yyyy-MM-dd'T'HH:mm:ss
+    // yyyy-MM-dd'T'HH:mm:ss.SSS
+    // yyyy-MM-dd'T'HH:mm:ss.SSS'Z'
+    protected static final DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .append(ISO_LOCAL_DATE)
+            .optionalStart()
+            .appendLiteral(' ')        // ' ' made optional
+            .optionalEnd()
+            .optionalStart()
+            .appendLiteral('T')        // 'T' made optional
+            .optionalEnd()
+            .optionalStart()
+            .append(ISO_LOCAL_TIME)    // time made optional
+            .optionalEnd()
+            .optionalStart()           // zone and offset made optional
+            .appendOffsetId()
+            .optionalEnd()
+            .optionalStart()
+            //为了兼容时区不带:这种格式
+            .appendOffset("+HHMM", "Z")
+            .optionalEnd()
+            .optionalStart()
+            .appendLiteral('[')
+            .parseCaseSensitive()
+            .appendZoneRegionId()
+            .appendLiteral(']')
+            .optionalEnd()
+            .toFormatter();
     private String pattern = "yyyy-MM-dd HH:mm:ss";
 
     // 方案一
     // 指定反序列化支持的日期格式，如前端传递字符串给Controller
     @Bean
     public LocalDateTimeDeserializer localDateTimeDeserializer() {
-        return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(pattern));
+        return new LocalDateTimeDeserializer(dateTimeFormatter);
     }
 
     // 指定序列化支持的日期格式，如Controller返回日期
