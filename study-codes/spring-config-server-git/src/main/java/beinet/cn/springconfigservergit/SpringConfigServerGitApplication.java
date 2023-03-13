bdfe12629f548b9e4cb2f531a6202b230b33466b
beiliang_you <beiliang_you@fzzixun.com>
2023-03-13 16:58:51 +0800

    spring配置中心demo：配置中心服务端项目完成，可以直接启动，并等待客户端接入

--- study-codes/spring-config-server-git/src/main/java/beinet/cn/springconfigservergit/SpringConfigServerGitApplication.java
+++ study-codes/spring-config-server-git/src/main/java/beinet/cn/springconfigservergit/SpringConfigServerGitApplication.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringConfigServerGitApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
