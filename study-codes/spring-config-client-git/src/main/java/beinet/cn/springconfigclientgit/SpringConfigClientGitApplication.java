0d8abb4af551666ccdeec51f048fb812fc628940
beiliang_you <beiliang_you@fzzixun.com>
2023-05-26 10:12:31 +0800

    spring cloud配置中心客户端空项目创建

---
+++ study-codes/spring-config-client-git/src/main/java/beinet/cn/springconfigclientgit/SpringConfigClientGitApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.springconfigclientgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigClientGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigClientGitApplication.class, args);
+    }
+
+}
---
+++ study-codes/spring-config-client-git/src/test/java/beinet/cn/springconfigclientgit/SpringConfigClientGitApplicationTests.java
@@ -0,0 +1,13 @@
+package beinet.cn.springconfigclientgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
