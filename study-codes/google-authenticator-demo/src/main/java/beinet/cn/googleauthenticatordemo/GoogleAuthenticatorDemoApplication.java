a27a96cc1779fc5941cc8f223e74f965a3323633
beiliang_you <beiliang_you@fzzixun.com>
2023-05-30 11:56:39 +0800

    谷歌身份验证器Google Authenticator Demo：项目新建，无任何实现

---
+++ study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/GoogleAuthenticatorDemoApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.googleauthenticatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleAuthenticatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleAuthenticatorDemoApplication.class, args);
+	}
+
+}
---
+++ study-codes/google-authenticator-demo/src/test/java/beinet/cn/googleauthenticatordemo/GoogleAuthenticatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package beinet.cn.googleauthenticatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleAuthenticatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
