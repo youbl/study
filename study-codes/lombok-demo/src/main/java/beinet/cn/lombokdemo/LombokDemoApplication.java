0882f00091d9f90a8d5b9eeba023fbb626603e2b
beiliang_you <beiliang_you@fzzixun.com>
2023-06-02 11:17:01 +0800

    lombok演示Demo：空项目，没有其它代码

---
+++ study-codes/lombok-demo/src/main/java/beinet/cn/lombokdemo/LombokDemoApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.lombokdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokDemoApplication.class, args);
+    }
+
+}
