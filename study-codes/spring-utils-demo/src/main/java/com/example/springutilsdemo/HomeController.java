da6119dbf55a4adb9199eca5247e339ff2d97de0
beiliang_you <beiliang_you@fzzixun.com>
2023-07-05 14:07:30 +0800

    org.springframework.util工具库演示项目：空项目创建

---
+++ study-codes/spring-utils-demo/src/main/java/com/example/springutilsdemo/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.springutilsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * 新类
+ *
+ * @author youbl
+ * @since 2023/5/30 11:52
+ */
+@RestController
+public class HomeController {
+    @GetMapping("")
+    public String index() {
+        return this.getClass().getName() + " " + LocalDateTime.now();
+    }
+}
---
+++ study-codes/spring-utils-demo/src/main/java/com/example/springutilsdemo/SpringUtilsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springutilsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUtilsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUtilsDemoApplication.class, args);
+	}
+
+}
