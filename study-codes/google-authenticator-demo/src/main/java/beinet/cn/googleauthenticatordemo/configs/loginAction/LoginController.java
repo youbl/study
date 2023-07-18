20ad44f0ad33f99d13cf5c930fc45840982af855
beiliang_you <youbl@126.com>
2023-07-18 15:43:15 +0800

    谷歌身份验证器Google Authenticator Demo：代码重构优化，LoginService不处理请求上下文的逻辑

--- study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/configs/loginAction/LoginController.java
+++ study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/configs/loginAction/LoginController.java
@@ -1,8 +1,10 @@
 package beinet.cn.googleauthenticatordemo.configs.loginAction;
 
+import beinet.cn.googleauthenticatordemo.utils.ContextUtil;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,16 +27,57 @@ public class LoginController {
     public void doLogin(@RequestBody(required = false) LoginDto dto,
                         HttpServletRequest request,
                         HttpServletResponse response) {
-        String username;
-        String pwd;
-        if (dto == null || !StringUtils.hasLength(dto.getBeinetUser())) {
-            username = request.getParameter("beinetUser");
-            pwd = request.getParameter("beinetPwd");
-        } else {
-            username = dto.getBeinetUser();
-            pwd = dto.getBeinetPwd();
+        dto = getUserPwd(dto, request);
+
+        // 登录失败
+        if (!loginService.processLogin(dto.getBeinetUser(), dto.getBeinetUser())) {
+            addTokenCookie("", response);
+            ContextUtil.endResponse(request, response, "账号或密码错误");
+            return;
         }
-        loginService.processLogin(request, response, username, pwd);
+
+        // 账号密码比对成功，写入Cookie
+        String token = TokenHelper.buildToken(dto.getBeinetUser());
+        addTokenCookie(token, response);
+
+        if (!ContextUtil.isAjax(request)) {
+            // 页面请求，进行302跳转
+            String redirectUrl = request.getParameter("url");
+            if (!StringUtils.hasLength(redirectUrl))
+                redirectUrl = "index.html";
+            ContextUtil.redirect(response, redirectUrl);
+        } else {
+            // ajax请求，返回json数据
+            ContextUtil.writeJson(response, 0, dto.getBeinetUser() + " 登录成功", token);
+        }
+    }
+
+    @GetMapping("logout")
+    public void doLogout(HttpServletResponse response) {
+        // 退出登录，注：这里没有清理两步验证的cookie，这样下次这台电脑登录可以不需要两步验证了
+        // 你可以根据自己的业务需要，选择清理
+        addTokenCookie("", response);
+    }
+
+    private LoginDto getUserPwd(LoginDto dto, HttpServletRequest request) {
+        if (dto == null) {
+            dto = new LoginDto();
+        }
+        if (!StringUtils.hasLength(dto.getBeinetUser())) {
+            dto.setBeinetUser(request.getParameter("beinetUser"));
+            dto.setBeinetPwd(request.getParameter("beinetPwd"));
+        }
+        return dto;
+    }
+
+    /**
+     * 为响应添加登录cookie
+     *
+     * @param token    登录token，为空表示删除cookie
+     * @param response 响应上下文
+     */
+    private void addTokenCookie(String token, HttpServletResponse response) {
+        ContextUtil.addCookie(TokenHelper.TOKEN_COOKIE_NAME, token, response);
     }
 
     @Data
--- study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/configs/loginAction/LoginService.java
+++ study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/configs/loginAction/LoginService.java
@@ -1,13 +1,9 @@
 package beinet.cn.googleauthenticatordemo.configs.loginAction;
 
-import beinet.cn.googleauthenticatordemo.utils.ContextUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 新类
@@ -22,35 +18,17 @@ public class LoginService {
 
     /**
      * 根据用户名密码，并进行登录
-     *
-     * @param request  请求上下文
-     * @param response 响应上下文
      */
-    public void processLogin(HttpServletRequest request, HttpServletResponse response, String username, String pwd) {
+    public boolean processLogin(String username, String pwd) {
         log.debug("用户名: {}, 准备登录", username);
         if (!validateUser(username, pwd)) {
             log.debug("用户名: {}, 账号或密码错误", username);
             //throw new RuntimeException("账号或密码错误");
-            addTokenCookie("", response);
-            ContextUtil.endResponse(request, response, "账号或密码错误");
-            return;
+            return false;
         }
 
         log.debug("用户名: {}, 账号密码认证成功", username);
-        String token = TokenHelper.buildToken(username);
-        addTokenCookie(token, response);
-
-        String redirectUrl = request.getParameter("url");
-        if (!StringUtils.hasLength(redirectUrl))
-            redirectUrl = "index.html";
-
-        if (!ContextUtil.isAjax(request)) {
-            // 页面请求，进行302跳转
-            ContextUtil.redirect(response, redirectUrl);
-        } else {
-            // ajax请求，返回json数据
-            ContextUtil.writeJson(response, 0, username + " 登录成功", token);
-        }
+        return true;
     }
 
     /**
@@ -60,7 +38,7 @@ public class LoginService {
      * @param pwd      密码
      * @return 是否正确
      */
-    public boolean validateUser(String username, String pwd) {
+    private boolean validateUser(String username, String pwd) {
         if (!StringUtils.hasLength(username) || !StringUtils.hasLength(pwd)) {
             return false;
         }
@@ -68,14 +46,5 @@ public class LoginService {
         return username.equalsIgnoreCase("beinet") && pwd.equals("beinet");
     }
 
-    /**
-     * 为响应添加登录cookie
-     *
-     * @param token    登录token，为空表示删除cookie
-     * @param response 响应上下文
-     */
-    public static void addTokenCookie(String token, HttpServletResponse response) {
-        ContextUtil.addCookie(TokenHelper.TOKEN_COOKIE_NAME, token, response);
-    }
 
 }
