bbd428424153491e6a322da3701af2bc2d963beb
beiliang_you <youbl@126.com>
2023-09-02 11:25:01 +0800

    尝试遍历ldap属性

--- study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/configs/loginAction/LoginService.java
+++ study-codes/google-authenticator-demo/src/main/java/beinet/cn/googleauthenticatordemo/configs/loginAction/LoginService.java
@@ -2,11 +2,19 @@ package beinet.cn.googleauthenticatordemo.configs.loginAction;
 
 import beinet.cn.googleauthenticatordemo.utils.SpringUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.filter.EqualsFilter;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.InitialLdapContext;
 
 /**
  * 新类
@@ -65,14 +73,57 @@ public class LoginService {
 
         if (username.indexOf("@") <= 0)
             username += "@beinet.cn"; // 按ldap的uid规则拼接
-        
+
         try {
             Object context = ldapTemplate.getContextSource().getContext(username, pwd);
             // log.info(context.toString());
+
+            log.info("准备遍历LDAP的所有属性：");
+            enumAllAttr(context, username);
             return true;
         } catch (Exception exp) {
             log.error(exp.getMessage());
             return false;
+        }
+    }
+
+    @SneakyThrows
+    private void enumAllAttr(Object objContext, String username) {
+        if (!(objContext instanceof InitialLdapContext)) {
+            return;
+        }
+        InitialLdapContext context = (InitialLdapContext) objContext;
+        NamingEnumeration<? extends Attribute> atts = context.getAttributes("").getAll();
+        log.info("第一种方式遍历打印：");
+        while(atts.hasMore()){
+            Attribute att = atts.next();
+            System.out.println(att.getID() + ": " + att.get());
+        }
+
+
+        String filter = new EqualsFilter("uid", username).toString();
+        SearchControls searchControls = new SearchControls();
+        searchControls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        searchControls.setReturningAttributes(new String[0]); // 返回所有属性
+
+        NamingEnumeration<SearchResult> results = context.search("", filter, searchControls);
+
+        log.info("第二种方式遍历打印：");
+        // 遍历搜索结果
+        while (results.hasMore()) {
+            SearchResult searchResult = results.next();
+
+            // 获取用户的属性
+            Attributes attributes = searchResult.getAttributes();
+            NamingEnumeration<String> attributeNames = attributes.getIDs();
+
+            while (attributeNames.hasMore()) {
+                String attributeName = attributeNames.next();
+                Attribute attribute = attributes.get(attributeName);
+
+                // 打印属性名和属性值
+                System.out.println(attributeName + ": " + attribute.get());
+            }
         }
     }
 
