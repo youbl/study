05e86c2c93a030c9f764c390435ba226d89fc83b
beiliang_you <youbl@126.com>
2023-11-10 17:26:04 +0800

    增加获取url header的代码，是HEAD方法，不是OPTIONS方法

--- study-codes/spring-utils-demo/src/main/java/com/example/springutilsdemo/HomeController.java
+++ study-codes/spring-utils-demo/src/main/java/com/example/springutilsdemo/HomeController.java
@@ -2,9 +2,12 @@ package com.example.springutilsdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 新类
@@ -23,5 +26,11 @@ public class HomeController {
     public String is404() {
         // 所有404请求，都要到这里
         return "404页面找不到";
+    }
+
+    @GetMapping("header")
+    public static Map<String, List<String>> getHeaders(@RequestParam String url) {
+        Map<String, List<String>>  ret = HttpUtils.getHeaders(url);
+        return ret;
     }
 }
