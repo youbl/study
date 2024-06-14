9145b851b2c3f3a5e4ad9dc11535dc86d2ae5823
beiliang_you <youbl@126.com>
2024-06-14 15:32:19 +0800

    从Windows Chrome中读取存储的账号密码数据并输出的代码，补充一些说明

--- study-codes/chrome-descrypt/src/main/java/beinet/cn/chromedescrypt/ChromeDescryptApplication.java
+++ study-codes/chrome-descrypt/src/main/java/beinet/cn/chromedescrypt/ChromeDescryptApplication.java
@@ -7,13 +7,37 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChromeDescryptApplication {
 
     public static void main(String[] args) {
-        String localStateFile = "D:/Local State"; // 解密密钥文件路径
-        String localPwdDB = "D:/Login Data";      // 账号密码的sqlite文件路径
+        readAndShowChromePassword();
+
+        //SpringApplication.run(ChromeDescryptApplication.class, args);
+    }
+
+    /**
+     * 读取并解密Chrome存储的账号密码
+     */
+    private static void readAndShowChromePassword() {
+        /*
+         * 账号密码存储的文件路径，在
+         * "%LocalAppData%\Google\Chrome\User Data\Default\Login Data"
+         * 找不到时，也可以在chrome里访问 chrome://version/ ，页面查找Profile Path
+         * 这是一个sqlite数据库文件.
+         * 注：建议关闭Chrome后再读取，也可以把它拷贝出来进行读取测试
+         */
+        String localPwdDB = "D:/Login Data";
+        /*
+         * 上面的密码是加密了的，解密的密码放在另一个文件里，文件路径在
+         * %LocalAppData%\Google\Chrome\User Data\Local State"
+         * 这是一个json格式的文件，密码在 os_crypt.encrypted_key 属性里.
+         *
+         * 注意：只能在本机运行该程序，解密其它机器上的文件会抛异常：
+         * Exception in thread "main" com.sun.jna.platform.win32.Win32Exception: 该项不适于在指定状态下使用。
+         */
+        String localStateFile = "D:/Local State";
         var result = new ChromeUserDataReader(localPwdDB, localStateFile).queryData();
         for (var item : result) {
+            // 输出解密后的账号密码
             System.out.println(item);
         }
-        //SpringApplication.run(ChromeDescryptApplication.class, args);
     }
 
 }
