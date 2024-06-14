3cf7635f65f1a3143092cb37d2436805d2d1ff1b
beiliang_you <youbl@126.com>
2024-06-14 14:28:15 +0800

    chrome账号密码和cookie读取项目创建

---
+++ study-codes/chrome-descrypt/src/main/java/beinet/cn/chromedescrypt/ChromeDescryptApplication.java
@@ -0,0 +1,13 @@
+package beinet.cn.chromedescrypt;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromeDescryptApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        //SpringApplication.run(ChromeDescryptApplication.class, args);
+    }
+
+}
