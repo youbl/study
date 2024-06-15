dd874c2030e66a8dda8fa34467075920aca6a68c
beiliang_you <youbl@126.com>
2024-06-15 19:43:46 +0800

    添加新说明和代码，可以连接已经打开了的chrome

--- python/selenium_webdriver/openNewChromeAndExecuteBaiduSearch.py
+++ python/selenium_webdriver/openNewChromeAndExecuteBaiduSearch.py
@@ -14,6 +14,9 @@ import traceback,time
 def main():
     try:
         driver = openNewChrome()
+
+        # create new tab and navigate to baidu
+        driver.switch_to.new_window('tab')
         driver.get('https://www.baidu.com')
         # wait for page loaded
         wait = WebDriverWait(driver, 10)
@@ -28,6 +31,11 @@ def main():
         cond = (By.CSS_SELECTOR, 'input[type="submit"]')
         submit_button = wait.until(EC.element_to_be_clickable(cond))
         submit_button.click()
+
+        # google.com use these 3 line code
+        #element = driver.find_element('css selector', '[name="q"]')
+        #element.send_keys("MoreLogin")
+        #element.send_keys(Keys.RETURN)
         print('search executed')
 
         time.sleep(15)
@@ -42,13 +50,23 @@ def main():
 
 #
 def openNewChrome():
-    # no arg means default chrome, or you can specified a path to chrome: webdriver.Chrome('/path/to/chromedriver')
+    # no arg means use default chromedriver
     driver = webdriver.Chrome()
     
     print('debug url:', driver.command_executor._url)    
     print('opened chrome session id:', driver.session_id)
     
     return driver
+
+# create webdriver with exist browser, debug_url format: 127.0.0.1:12345
+def createWebDriverWithUrl(debug_url):
+    opts = webdriver.ChromeOptions()
+    # opts.set_capability('browserVersion', str(version))
+    opts.add_experimental_option('debuggerAddress', debug_url)
+    driver = webdriver.Chrome(options=opts)
+    print(driver.current_url)
+    return driver
+
 
 if __name__ == '__main__':
     main()
