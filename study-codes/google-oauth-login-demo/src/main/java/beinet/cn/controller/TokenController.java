6d8fde2f43b2f0e4b9343f987d2a766fa0cf6721
beiliang_you <youbl@126.com>
2025-02-13 11:57:08 +0800

    第4种谷歌登录方案：前端拿到google的code后，通过后端获取google用户信息的代码

--- study-codes/google-oauth-login-demo/src/main/java/beinet/cn/controller/TokenController.java
+++ study-codes/google-oauth-login-demo/src/main/java/beinet/cn/controller/TokenController.java
@@ -4,6 +4,8 @@ import beinet.cn.utils.GoogleJwtUtils;
 import beinet.cn.utils.dto.GoogleUser;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,14 @@ public class TokenController {
     @SneakyThrows
     public GoogleUser token(@RequestParam String accessToken) {
         return GoogleJwtUtils.getUserInfoByToken(accessToken);
+    }
+
+    @GetMapping("google/code")
+    @SneakyThrows
+    public GoogleUser code(@RequestParam String code, @RequestParam String callbackUrl) {
+        String accessToken = GoogleJwtUtils.getAccessToken(code, callbackUrl);
+        Assert.isTrue(StringUtils.hasLength(accessToken), "取token失败");
+
+        return token(accessToken);
     }
 }
--- study-codes/google-oauth-login-demo/src/main/java/beinet/cn/utils/GoogleJwtUtils.java
+++ study-codes/google-oauth-login-demo/src/main/java/beinet/cn/utils/GoogleJwtUtils.java
@@ -10,7 +10,10 @@ import com.google.api.client.json.gson.GsonFactory;
 import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.experimental.Accessors;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +28,8 @@ import java.util.Collections;
  */
 public class GoogleJwtUtils {
     private final static String CLIENT_ID = "1093585410643-qi1pdejjt5urqevtd3o4k0joilg02711.apps.googleusercontent.com";
+    private final static String CLIENT_SCREPT = "GOOGLE后台查找，跟ClientID一起的那个Secret";
+
     private final static RestTemplate restTemplate = new RestTemplate();
 
     @SneakyThrows
@@ -60,6 +65,37 @@ public class GoogleJwtUtils {
         GoogleInfoResult result = restTemplate.getForObject(url, GoogleInfoResult.class);
         Assert.notNull(result, "Google token is not valid");
         return convertToGoogleUser(result);
+    }
+
+
+    /**
+     * 根据google返回的code，和获取code时使用的callbackUrl，去获取accessToken
+     * 参考： <a href="https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow?hl=zh-cn#handlingresponse">...</a>
+     * 测试用这个url获取code
+     * <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email&access_type=offline&include_granted_scopes=true&response_type=code&state=aaa&redirect_uri=https%3A//cb-gw-pre.zibird.com/app/google/callback&client_id=1093585410643-qi1pdejjt5urqevtd3o4k0joilg02711.apps.googleusercontent.com">...</a>
+     * @param code code
+     * @param callbackUrl 获取code时使用的callbackUrl，必须匹配，否则会出错
+     * @return accessToken
+     */
+    public static String getAccessToken(String code, String callbackUrl) {
+        // 设置请求参数
+        MultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+        requestParams.add("code", code);
+        requestParams.add("client_id", CLIENT_ID);
+        requestParams.add("client_secret", CLIENT_SCREPT);
+        requestParams.add("redirect_uri", callbackUrl);
+        requestParams.add("grant_type", "authorization_code");
+
+        // 获取token的url
+        String tokenUrl = "https://oauth2.googleapis.com/token";
+        // 发送POST请求
+        ResponseEntity<OAuth2Token> response = restTemplate.postForEntity(tokenUrl, requestParams, OAuth2Token.class);
+
+        // 返回响应体
+        if (response.getBody() != null) {
+            return response.getBody().getAccess_token();
+        }
+        return null;
     }
 
     private static GoogleUser convertToGoogleUser(GoogleIdToken.Payload payload) {
@@ -101,5 +137,14 @@ public class GoogleJwtUtils {
         private String given_name;
         private String family_name;
         private String picture;
+    }
+
+    @Data
+    static class OAuth2Token {
+        private String access_token;
+        private Long expires_in;
+        private String scope;
+        private String token_type;
+        private String id_token;
     }
 }
