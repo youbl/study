c2d5a7709121f4ec3cafce12942d0855f1595352
beiliang_you <youbl@126.com>
2025-04-08 16:22:09 +0800

    fixedDelay是从任务执行完成才开始计时； 而fixedRate是从任务启动就开始计时，等5秒到达且上次执行完成才启动下次任务

--- study-codes/spring-scheduled-study/src/main/java/beinet/cn/springscheduledstudy/FixedScheduledTask.java
+++ study-codes/spring-scheduled-study/src/main/java/beinet/cn/springscheduledstudy/FixedScheduledTask.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class FixedScheduledTask {
 
-    /* 注：默认只有一个Schedule线程，所以第1个方法也会被迫等7秒后执行 */
+    /*
+     * 注：默认只有一个Schedule线程，所以第1个方法也会被迫等7秒后执行,
+     * 需要添加配置：spring.task.scheduling.pool.size=5
+     *  */
 
     // 首次启动就执行，下次要等5秒后执行
     @Scheduled(fixedRate = 5000)
@@ -19,10 +22,20 @@ public class FixedScheduledTask {
     }
 
     // 首次启动就执行，因为有sleep，导致下次要等7秒后执行
+    // Spring对同一个任务，默认不会并行执行，因此不会固定等5秒，而是在5秒到达且上次执行完成才启动第2次
     @Scheduled(fixedRate = 5000)
     @SneakyThrows
     void fixedRateTaskWithSleep() {
         log.info("fixedRateTaskWithSleep 5000开始");
+        Thread.sleep(7000);
+    }
+
+    // 首次启动就执行，方法执行结束后，再等5秒后执行
+    // 因为方法里休眠了7秒，所以第2次执行实际要等12秒
+    //@Scheduled(fixedDelay = 5000)
+    @SneakyThrows
+    void fixedDelayTask() {
+        log.info("fixedDelayTask 5000开始");
         Thread.sleep(7000);
     }
 }
