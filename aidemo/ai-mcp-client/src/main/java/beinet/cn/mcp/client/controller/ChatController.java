aca40d9c5dea4b9727fda3cd6cb38d1a888171a1
beiliang_you <youbl@126.com>
2025-04-21 17:54:09 +0800

    Spring-ai的MCP-Client实现，改用deepseek大模型

--- aidemo/ai-mcp-client/src/main/java/beinet/cn/mcp/client/controller/ChatController.java
+++ aidemo/ai-mcp-client/src/main/java/beinet/cn/mcp/client/controller/ChatController.java
@@ -2,10 +2,12 @@ package beinet.cn.mcp.client.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -22,6 +24,9 @@ public class ChatController {
 
     @GetMapping("chat")
     public Map<String, String> chat(@RequestParam String msg) {
+        if (!StringUtils.hasText(msg)) {
+            return new HashMap<>();
+        }
         String response = chatClient.prompt().user(msg).call().content();
         assert response != null;
         return Map.of("response", response);
